5152f51f77480eef45717ef356afe586ba1edd66
Muhammad Umair Khan <umair.anwar@xflowresearch.com>
2026-09-12 21:06:14 +0500

    feat: Integrate Sentry error tracking for Rust and React

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    let _guard = sentry::init(("https://93c09cda03ee28115f0fcd5462523775@o4511788661669888.ingest.de.sentry.io/4512074160799824", sentry::ClientOptions {
+        release: sentry::release_name!(),
+        traces_sample_rate: 1.0,
+        ..Default::default()
+    }));
+
     ssh_manager_tauri_lib::run()
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://93c09cda03ee28115f0fcd5462523775@o4511788661669888.ingest.de.sentry.io/4512074160799824",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+});
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />

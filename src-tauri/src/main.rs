13d1b730bc15426edd76a76729a5306669a814a8
Muhammad Umair Khan <umair.anwar@xflowresearch.com>
2026-09-12 21:10:12 +0500

    Fix dashboard SSH Keys count to dynamically load from backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,11 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    let _guard = sentry::init(("https://93c09cda03ee28115f0fcd5462523775@o4511788661669888.ingest.de.sentry.io/4512074160799824", sentry::ClientOptions {
-        release: sentry::release_name!(),
-        traces_sample_rate: 1.0,
-        ..Default::default()
-    }));
+    let mut options = sentry::ClientOptions::default();
+    options.release = sentry::release_name!();
+    let _guard = sentry::init((
+        "https://93c09cda03ee28115f0fcd5462523775@o4511788661669888.ingest.de.sentry.io/4512074160799824",
+        options,
+    ));
 
     ssh_manager_tauri_lib::run()
 }
